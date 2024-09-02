71233432ce898a5415c787339a213fab25384b45
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-09-02 14:45:15 +0800

    feat: add sveltekit template

---
+++ svelte/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Description for the project";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            litecli
+            nodePackages.nodejs
+            nodePackages.pnpm
+            nodePackages.svelte-language-server
+          ];
+          shellHook = ''
+            export ESLINT_USE_FLAT_CONFIG="true"
+          '';
+        };
+      };
+    };
+}
+
